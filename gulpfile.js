a4b3091d879db6526e294e7d4b53a5bd5498473a
Katrina Nov <katrina.nov13@gmail.com>
2019-12-02 10:58:52 +0300

    add minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
 
 // Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch("./*.html").on('change', browserSync.reload);
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "src"
+    }
+  });
+  gulp.watch("src/*.html").on('change', browserSync.reload);
 });
+// минифицировать css
+gulp.task('minify-css', () => {
+  return gulp.src('src/css/*.css')
+    .pipe(rename({
+      suffix: '.min',
+      prefix: ''
+    }))
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch("src/css/*.css").on('change', browserSync.reload);
+})
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'minify-css'));
