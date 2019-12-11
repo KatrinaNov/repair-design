4a2957ff318fb8e03c203b74bb226aabee7e20a8
Katrina Nov <katrina.nov13@gmail.com>
2019-12-11 12:48:05 +0300

    feat: js function for modal

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  const modal = document.querySelector('.modal');
+  // получаем все кнопки, которые имею атрибут data-toggle равный modal (кнопки которые должны открывать модальное окно)
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+
+  const switchModal = () => {
+    modal.classList.toggle('modal_visible');
+  }
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+});
