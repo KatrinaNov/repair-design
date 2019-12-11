37a72a0bcecdb4980a0079159dd5e1ab3ccbd833
Katrina Nov <katrina.nov13@gmail.com>
2019-12-11 13:56:06 +0300

    feat: add closing by esc and by clicking out of modal dialog

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   // получаем все кнопки, которые имею атрибут data-toggle равный modal (кнопки которые должны открывать модальное окно)
+  const modalDialog = document.querySelector('.modal__dialog');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
 
@@ -8,11 +9,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const switchModal = () => {
     modal.classList.toggle('modal_visible');
   }
-  
+  const removeModal = () => {
+    modal.classList.remove('modal_visible');
+  }
+
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
 
   closeBtn.addEventListener('click', switchModal);
 
+  document.addEventListener('keydown', function (e) {
+    if (e.code == "Escape") removeModal();
+  }); 
+  document.addEventListener('click', (e) => {
+    if (e.target == modal) removeModal();
+  });
 });
