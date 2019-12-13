c9e2c4c0951cca0f102bc0e92f31011624586b53
Katrina Nov <katrina.nov13@gmail.com>
2019-12-13 20:22:26 +0300

    feat: add responsive for projects

--- src/js/main.js
+++ src/js/main.js
@@ -84,7 +84,7 @@ $(document).ready(function () {
   var prev = $('.swiper-button-prev');
   var bullets = $('.swiper-pagination');
 
-  next.css('left', prev.width() + bullets.width()+60)
+  next.css('left', prev.width() + bullets.width() + 60)
   bullets.css('left', prev.width() + 30)
 
 
